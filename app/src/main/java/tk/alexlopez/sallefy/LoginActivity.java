d3ba1a83cd1927533e787ffe3b658f55f32bd083
AlexLopezDevelop <alex_lopez.es@icloud.com>
2020-03-01 16:26:43 +0100

    creating: login activity design

--- app/src/main/java/tk/alexlopez/sallefy/LoginActivity.java
+++ app/src/main/java/tk/alexlopez/sallefy/LoginActivity.java
@@ -3,12 +3,21 @@ package tk.alexlopez.sallefy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    VideoView mVideoLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mVideoLogin = findViewById(R.id.videoLogin);
+
+        String videoLoginPath = "android.resource://"+getPackageName()+"/"+R.raw.login_video;
+        mVideoLogin.setVideoPath(videoLoginPath);
+        mVideoLogin.start();
     }
 }
