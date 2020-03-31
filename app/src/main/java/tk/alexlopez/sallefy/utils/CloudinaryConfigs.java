0081e0cdca23efff1a72963849db4a534a0075a2
Moha_ <ahrir@live.com>
2020-03-31 18:16:09 +0200

    cloudinary account created and test upload song OK

--- app/src/main/java/tk/alexlopez/sallefy/utils/CloudinaryConfigs.java
+++ app/src/main/java/tk/alexlopez/sallefy/utils/CloudinaryConfigs.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public class CloudinaryConfigs {
 
     public static HashMap getConfigurations() {
-        
+        HashMap  config = new HashMap();
+        config.put("cloud_name", "android-moha" );
+        config.put("api_key", "985947289876736");
+        config.put("api_secret", "LZ9WUFwHP8ARIXqTe4nOek-Z0jg");
+        return config;
     }
 }
