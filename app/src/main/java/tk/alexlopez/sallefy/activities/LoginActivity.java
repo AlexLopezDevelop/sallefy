1a6334eea2910ef4f15b6ab9c246c386ffa51db6
Alex Lopez <alex_lopez.es@icloud.com>
2020-04-12 12:23:09 +0200

    finished: #4

--- app/src/main/java/tk/alexlopez/sallefy/activities/LoginActivity.java
+++ app/src/main/java/tk/alexlopez/sallefy/activities/LoginActivity.java
@@ -94,7 +94,8 @@ public class LoginActivity extends AppCompatActivity implements UserCallback {
         Session.getInstance(getApplicationContext())
                 .setUserToken(userToken);
         //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        Intent intent = new Intent(getApplicationContext(), TracksListActivity.class);
+        intent.putExtra("id", 8);
         startActivity(intent);
     }
 
