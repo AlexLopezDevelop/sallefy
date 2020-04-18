c1c2ba1f10cf1a0304d4311e7bbb3b75668b8599
Alex Molero Nicolas <alex.molero@students.salle.url.edu>
2020-04-18 19:13:11 +0200

    #9 UI search Playlist Done

--- app/src/main/java/tk/alexlopez/sallefy/fragments/SearchFragment.java
+++ app/src/main/java/tk/alexlopez/sallefy/fragments/SearchFragment.java
@@ -1,9 +1,15 @@
 package tk.alexlopez.sallefy.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,6 +20,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 
 import tk.alexlopez.sallefy.R;
+import tk.alexlopez.sallefy.activities.SignupActivity;
 import tk.alexlopez.sallefy.adapters.TrackListAdapter;
 import tk.alexlopez.sallefy.network.manager.MeManager;
 import tk.alexlopez.sallefy.network.manager.PlaylistManager;
@@ -22,6 +29,7 @@ import tk.alexlopez.sallefy.network.manager.PlaylistManager;
 public class SearchFragment extends Fragment {
 
     public static final String TAG = SearchFragment.class.getName();
+    private EditText input_search;
 
 
 
@@ -56,7 +64,28 @@ public class SearchFragment extends Fragment {
 
     private void initViews(View v) {
 
+        input_search = (EditText) v.findViewById(R.id.input_search);
+        input_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                Log.d(TAG, "HAY CAMBIOOOOOOOOOOOOOOOOOOOOO " );
+
+                // you can call or do what you want with your EditText here
+
+                // yourEditText...
+            }
+        });
     }
 
     private void getData() {
