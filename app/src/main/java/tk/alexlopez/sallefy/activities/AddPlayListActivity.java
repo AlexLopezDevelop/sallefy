5f9288509b45cbe81bb5b401619549b55e4afc62
David Casaoliva Rey <david.casaoliva@students.salle.url.edu>
2020-04-19 18:05:19 +0200

    #8 Created activity AddPlayListActivity

--- app/src/main/java/tk/alexlopez/sallefy/activities/AddPlayListActivity.java
+++ app/src/main/java/tk/alexlopez/sallefy/activities/AddPlayListActivity.java
@@ -1,8 +1,20 @@
 package tk.alexlopez.sallefy.activities;
 
-public class AddPlayListActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import tk.alexlopez.sallefy.R;
+
+public class AddPlayListActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addplaylist);
+
+    }
 
 
 
