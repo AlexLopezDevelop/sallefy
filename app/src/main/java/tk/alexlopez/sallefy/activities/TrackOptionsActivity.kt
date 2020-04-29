0c9aebb6922a6968b4861cc7a5c3bdf967df372e
Alex Lopez <alex_lopez.es@icloud.com>
2020-04-29 19:15:34 +0200

    updated: trackoption activity to kotlin

---
+++ app/src/main/java/tk/alexlopez/sallefy/activities/TrackOptionsActivity.kt
@@ -0,0 +1,54 @@
+package tk.alexlopez.sallefy.activities
+
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_track_options.*
+import tk.alexlopez.sallefy.R
+import tk.alexlopez.sallefy.models.Track
+
+class TrackOptionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_track_options)
+        initViews()
+        data
+    }
+
+    private fun initViews() {
+        like_track_option.setOnClickListener{
+
+        }
+
+        add_song_to_playlist_option.setOnClickListener {
+
+        }
+
+        show_artist_option.setOnClickListener {
+
+        }
+
+        delete_song_option.setOnClickListener {
+            
+        }
+    }
+
+    //.placeholder(R.drawable.ic_audiotrack) TODO: Change default image
+    private val data: Unit
+        private get() {
+            val intent = this.intent
+            val bundle = intent.extras
+            val track = bundle!!.getSerializable("trackData") as Track?
+            if (track!!.thumbnail != null) {
+                Glide.with(this)
+                        .asBitmap() //.placeholder(R.drawable.ic_audiotrack) TODO: Change default image
+                        .load(track.thumbnail)
+                        .into(track_cover!!)
+            }
+            track_title!!.text = track.name
+            track_author!!.text = track.userLogin
+        }
+}
