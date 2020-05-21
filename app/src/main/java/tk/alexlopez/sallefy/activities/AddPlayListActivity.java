6fcd6b62987681bd84195240fb714b98afd9722a
David Casaoliva Rey <david.casaoliva@students.salle.url.edu>
2020-05-21 17:59:32 +0200

    #14 Created AddPlayListActivity with search PlayList and add Playlist

--- app/src/main/java/tk/alexlopez/sallefy/activities/AddPlayListActivity.java
+++ app/src/main/java/tk/alexlopez/sallefy/activities/AddPlayListActivity.java
@@ -3,14 +3,18 @@ package tk.alexlopez.sallefy.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import java.awt.font.TextAttribute;
+import java.util.List;
 
 import tk.alexlopez.sallefy.R;
+import tk.alexlopez.sallefy.models.Playlist;
+import tk.alexlopez.sallefy.network.callback.FragmentCallback;
+import tk.alexlopez.sallefy.network.callback.PlaylistCallback;
 
-public class AddPlayListActivity extends AppCompatActivity {
+public class AddPlayListActivity extends AppCompatActivity implements PlaylistCallback, FragmentCallback {
 
-    private TextAttribute playlist;
+    private Playlist mPlaylists;
 
 
 
@@ -22,5 +26,20 @@ public class AddPlayListActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onPlaylistcreated() {
 
+    }
+
+    @Override
+    public void onPlaylistReceived(List<Playlist> tracks) {
+
+
+
+    }
+
+    @Override
+    public void onChangeFragment(Fragment fragment) {
+
+    }
 }
